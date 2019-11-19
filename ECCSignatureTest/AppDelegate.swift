cf23dafd0912967ce53544a475269a4ffbee2761
Danilo Campos <danilo@futurefluent.com>
2019-11-19 11:46:18 -0500

    Initial Commit

---
+++ ECCSignatureTest/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ECCSignatureTest
+//
+//  Created by Danilo Campos on 11/19/19.
+//  Copyright © 2019 Danilo Campos. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
